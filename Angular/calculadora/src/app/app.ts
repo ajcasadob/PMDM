a0c42bd64747cd5ec1d7951103f4a8baa31952f0
ajcasadob <ajcasbay@gmail.com>
2025-10-24 13:30:40 +0200

    done

---
+++ Angular/calculadora/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Calculo } from './calculo/calculo';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,Calculo],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('calculadora');
+}
---
+++ Angular/calculadora/src/app/calculo/calculo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculo',
+  imports: [],
+  templateUrl: './calculo.html',
+  styleUrl: './calculo.css',
+})
+export class Calculo {
+
+
+   numeros :number[] =[0,1,2,3,4,5,6,7,8,9];
+
+}
