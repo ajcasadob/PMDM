19c259f55d60db649803379108aecd3400005341
ajcasadob <ajcasbay@gmail.com>
2025-10-27 13:50:12 +0100

    done

--- Angular/calculadora/src/app/calculo/calculo.ts
+++ Angular/calculadora/src/app/calculo/calculo.ts
@@ -9,6 +9,102 @@ import { Component } from '@angular/core';
 export class Calculo {
 
 
-   numeros :number[] =[0,1,2,3,4,5,6,7,8,9];
+   num1 = 0;
+   num2 = 0;
+   result =0;
+   operator = '';
+   pulsado = false;
+   actualCalc='';
+
+   solveProblem(){
+  
+  if(this.operator == '+'){
+
+    this.suma();
+      return this.result;
+
+  }
+  if(this.operator == '-'){
+
+    this.resta();
+      return this.result;
+
+  }
+  if(this.operator == '*'){
+
+    this.multiplicacion();
+      return this.result;
+
+  }
+  if(this.operator == '/'){
+
+    this.division();
+      return this.result;
+
+  }
+
+  return 0;
+
+}
+
+clearAll(){
+  this.num1 = 0;
+  this.num2 = 0;
+  this.result = 0;
+  this.operator = '';
+  this.actualCalc = '';
+}
+
+asignNumber(number: number){
+
+  if(this.operator == ''){
+    this.num1 = parseInt(this.num1.toString() + number.toString());
+    this.actualCalc += number.toString();
+  }else{
+    this.num2 = parseInt(this.num2.toString() + number.toString());
+    this.actualCalc += number.toString();
+  }
+  
+}
+
+selectOperator(ope: string){
+
+  this.operator = ope;
+  this.actualCalc += this.operator;
+
+}
+
+suma(){
+
+  this.result = this.num1 + this.num2;
+  return this.result;
+
+}
+
+resta(){
+
+  this.result = this.num1 - this.num2;
+  return this.result;
+
+}
+
+multiplicacion(){
+
+  this.result = this.num1 * this.num2;
+  return this.result;
+
+}
+
+division(){
+
+  this.result = this.num1 / this.num2;
+  return this.result;
+
+}
+
+
+
+
+
 
 }
