2ab582aec3b21496f3f5dd3e279adc44dd1b9e79
ajcasadob <ajcasbay@gmail.com>
2025-11-10 13:40:08 +0100

    done

--- Angular/formulario2/src/app/app.ts
+++ Angular/formulario2/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterFormPage } from './register-form-page/register-form-page';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RegisterFormPage],
+  imports: [RegisterFormPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Angular/formulario2/src/app/register-form-page/register-form-page.ts
+++ Angular/formulario2/src/app/register-form-page/register-form-page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form-page',
@@ -9,25 +9,43 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class RegisterFormPage {
 
-  loginFormGroup = new FormGroup ({
-    emailFormControl: new FormControl(''),
-    passwordFormControl: new FormControl(''),
+
+  registerFormGroup = new FormGroup ({
+    nombre: new FormControl('',Validators.required),
+    apellidos: new FormControl('',Validators.required),
+    nif: new FormControl('',[Validators.required, Validators.pattern (/^[0-9]{8}[A-Za-z]$/)]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    telefono: new FormControl('',[Validators.required, Validators.pattern(/^[0-9]{9}$/)]),
+    sexo: new FormControl('', Validators.required),
+    conocido: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required),
+    confirmPassword: new FormControl('',Validators.required),
+    terminos: new FormControl(false, Validators.requiredTrue)
   });
 
 
   onLoginClicked(){
 
-    console.log('**Form Control inputs');
-    console.log('Email:',this.loginFormGroup.get('emailFormControl')?.value)
-    console.log('Password: ',this.loginFormGroup.get('passwordFormControl')?.value);
+    console.log('**Form Control inputs**');
+    console.log('Nombre:', this.registerFormGroup.get('nombre')?.value);
+    console.log('Apellidos:', this.registerFormGroup.get('apellidos')?.value);
+    console.log('NIF:', this.registerFormGroup.get('nif')?.value);
+    console.log('Email:', this.registerFormGroup.get('email')?.value);
+    console.log('Teléfono:', this.registerFormGroup.get('telefono')?.value);
+    console.log('Sexo:', this.registerFormGroup.get('sexo')?.value);
+    console.log('Conocido:', this.registerFormGroup.get('conocido')?.value);
+    console.log('Password:', this.registerFormGroup.get('password')?.value);
+    console.log('Confirm Password:', this.registerFormGroup.get('confirmPassword')?.value);
+    console.log('Términos:', this.registerFormGroup.get('terminos')?.value);
 
-    console.log('** Form group value **');
-    console.log('Form Value: ',this.loginFormGroup.value);
+    console.log('**Form group value**');
+    console.log('Form Value:', this.registerFormGroup.value);
   }
 
 
   clearForm(){
-    this.loginFormGroup.reset();
+    this.registerFormGroup.reset();
+
   }
 
 }
