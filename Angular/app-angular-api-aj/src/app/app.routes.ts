e793452c7ed12f2f63ed7c9313a943389f06b341
ajcasadob <ajcasbay@gmail.com>
2025-11-17 12:50:10 +0100

    done

--- Angular/app-angular-api-aj/src/app/app.routes.ts
+++ Angular/app-angular-api-aj/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { VehiclesListPage } from './vehicles-list-page/vehicles-list-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',
+    component: VehiclesListPage
+    }
+];
