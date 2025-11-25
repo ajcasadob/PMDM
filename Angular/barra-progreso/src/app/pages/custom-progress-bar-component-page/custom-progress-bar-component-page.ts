8901f915a5714915fbfef3c51dfadcbb0eb30770
ajcasadob <ajcasbay@gmail.com>
2025-11-25 13:18:57 +0100

    done

--- Angular/barra-progreso/src/app/pages/custom-progress-bar-component-page/custom-progress-bar-component-page.ts
+++ Angular/barra-progreso/src/app/pages/custom-progress-bar-component-page/custom-progress-bar-component-page.ts
@@ -1,18 +1,39 @@
 import { Component, Input, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-progress-bar-component-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './custom-progress-bar-component-page.html',
   styleUrl: './custom-progress-bar-component-page.css',
 })
 export class CustomProgressBarComponentPage {
 
+  progreso: number = 50;
+  colorSeleccionado: string = 'bg-primary';
+  striped: boolean = true;
+  animated: boolean = true;
+  showLabel: boolean = true;
 
-  @Input() value: number = 0;
-  @Input() color: string = 'bg-primary';
-  @Input() striped: boolean = false;
-  @Input() animated: boolean = false;
-  @Input() showLabel: boolean = false;
+  colores = [
+    { valor: 'bg-primary', nombre: 'Azul' },
+    { valor: 'bg-success', nombre: 'Verde' },
+    { valor: 'bg-danger', nombre: 'Rojo' },
+    { valor: 'bg-warning', nombre: 'Amarillo' },
+    { valor: 'bg-info', nombre: 'Cyan' },
+    { valor: 'bg-secondary', nombre: 'Gris' }
+  ];
+
+  aumentar(): void {
+    if (this.progreso < 100) {
+      this.progreso += 10;
+    }
+  }
+
+  disminuir(): void {
+    if (this.progreso > 0) {
+      this.progreso -= 10;
+    }
+  }
 
 }
